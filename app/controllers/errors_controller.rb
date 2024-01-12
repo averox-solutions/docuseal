99ab95f4147b7bd0fabd2ab2b726bd6f1cd56dbf
Imran <admin@averox.com>
2024-01-13 01:31:56 +0500

    SMTP

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,7 @@
 
 class ErrorsController < ActionController::Base
   ENTERPRISE_FEATURE_MESSAGE =
-    'This feature is available in Enterprise Edition: https://www.docuseal.co/pricing'
+    'This feature is available in Enterprise Edition: https://averox.com'
 
   ENTERPRISE_PATHS = [
     '/templates/html',
--- lib/docuseal.rb
+++ lib/docuseal.rb
@@ -4,7 +4,7 @@ module Docuseal
   PRODUCT_URL = 'https://www.docuseal.co'
   NEWSLETTER_URL = "#{PRODUCT_URL}/newsletters".freeze
   ENQUIRIES_URL = "#{PRODUCT_URL}/enquiries".freeze
-  PRODUCT_NAME = 'Averox'
+  PRODUCT_NAME = 'DocuSeal'
   DEFAULT_APP_URL = 'http://localhost:3000'
   GITHUB_URL = 'https://github.com/docusealco/docuseal'
   DISCORD_URL = 'https://discord.gg/qygYCDGck9'
